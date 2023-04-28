110003aa01810d7e7804fd75f2e0d947e8baf387
Renzo Meier <meierr14@students.zhaw.ch>
2023-04-28 16:46:35 +0200

    Kleine Korrektur des Erwarteten Rückgabewertes beim Test

--- demo/src/main/java/com/example/demo/leapYearChecker/LeapYearChecker.java
+++ demo/src/main/java/com/example/demo/leapYearChecker/LeapYearChecker.java
@@ -2,11 +2,15 @@ package com.example.demo.leapYearChecker;
 
 public class LeapYearChecker {
     
-    public String check(int i){
+    public String check(int i) {
         
-        if (i % 4 == 0 && i % 100 != 0 || i % 400 == 0){
-                    return "Leap year";
+        if (i < 0) {
+            return "No valid year!";
+        }else{
+            if (i % 4 == 0 && i % 100 != 0 || i % 400 == 0){
+                return "Leap year";
+            }
+            return "No leap year";
         }
-        return "No leap year";
-    }
+    }   
 }
