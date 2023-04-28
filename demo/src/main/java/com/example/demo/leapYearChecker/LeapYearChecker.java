e32b3347e0959722483e593cd370262cbe22ebed
Renzo Meier <meierr14@students.zhaw.ch>
2023-04-28 17:06:08 +0200

    Anpassung des Codes für Test Nr 10

--- demo/src/main/java/com/example/demo/leapYearChecker/LeapYearChecker.java
+++ demo/src/main/java/com/example/demo/leapYearChecker/LeapYearChecker.java
@@ -4,13 +4,13 @@ public class LeapYearChecker {
     
     public String check(int i) {
         
-        if (i <= 0) {
-            return "No valid year!";
-        }else{
+        if (0 < i && i < 10000) {
             if (i % 4 == 0 && i % 100 != 0 || i % 400 == 0){
                 return "Leap year";
             }
             return "No leap year";
+        }else{
+            return "No valid year!";
         }
     }   
 }
